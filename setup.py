d2df1ecfd79dfc46059a6501174c1e363cc2f2d4
sonyakun <90973770+sonyakun@users.noreply.github.com>
2022-10-15 20:52:55 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="sapt",
-    version="0.0.1",
+    version="0.0.2",
     description="aptコマンド支援ツール",
     author="sonyakun",
     packages=find_packages(),
